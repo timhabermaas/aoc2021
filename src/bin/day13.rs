5505bb171bae3dfcf4cbc7e796fc1fe5d88eea66
Tim Habermaas <tim.habermaas@gmail.com>
2021-12-13 12:25:39 +0100

    Day 13

---
+++ src/bin/day13.rs
@@ -0,0 +1,78 @@
+use std::collections::HashSet;
+use std::fs::read_to_string;
+
+fn main() {
+    let input = read_to_string("inputs/day13.txt").expect("file not found");
+
+    let mut coordinates = vec![];
+    let mut folds = vec![];
+
+    for l in input.lines() {
+        if l == "" {
+            continue;
+        }
+        if l.starts_with("fold") {
+            let mut parts = l.split_whitespace();
+            parts.next();
+            parts.next();
+            let c = parts.next().unwrap();
+            let mut parts = c.split("=");
+            folds.push((
+                parts.next().unwrap(),
+                parts.next().unwrap().parse::<u32>().unwrap(),
+            ));
+        } else {
+            let mut parts = l.split(",");
+            coordinates.push((
+                parts.next().unwrap().parse::<u32>().unwrap(),
+                parts.next().unwrap().parse::<u32>().unwrap(),
+            ));
+        }
+    }
+
+    let mut dots = coordinates.iter().copied().collect::<HashSet<_>>();
+
+    for fold in folds {
+        let f = fold.1;
+
+        let (before_fold, after_fold) = if fold.0 == "x" {
+            let (mut after_fold, before_fold): (HashSet<(u32, u32)>, HashSet<(u32, u32)>) =
+                dots.iter().partition(|(x, _y)| *x > f);
+
+            after_fold = after_fold.iter().map(|(x, y)| (f - (x - f), *y)).collect();
+
+            (before_fold, after_fold)
+        } else {
+            let (mut after_fold, before_fold): (HashSet<(u32, u32)>, HashSet<(u32, u32)>) =
+                dots.iter().partition(|(_x, y)| *y > f);
+
+            after_fold = after_fold.iter().map(|(x, y)| (*x, f - (y - f))).collect();
+
+            (before_fold, after_fold)
+        };
+
+        dots = before_fold
+            .union(&after_fold)
+            .copied()
+            .collect::<HashSet<_>>();
+
+        print(&dots);
+        println!("{}", dots.len());
+    }
+}
+
+fn print(set: &HashSet<(u32, u32)>) {
+    let max_y = *set.iter().map(|(_x, y)| y).max().unwrap();
+    let max_x = *set.iter().map(|(x, _y)| x).max().unwrap();
+
+    for y in 0..=max_y {
+        for x in 0..=max_x {
+            if set.contains(&(x, y)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!("");
+    }
+}
